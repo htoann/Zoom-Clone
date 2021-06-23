4d434250244572789a1094725cb523f0fd940fda
Trần Hữu Toàn <jungleoffice02@gmail.com>
2021-06-23 13:55:20 +0700

    Finish Zoom Clone

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
-
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
